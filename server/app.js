d54d499c3cb644d91c229e65464ca6c495ee5b8c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-07-21 13:53:33 +0700

    feat(server): add filter name based on query string "q"

--- server/app.js
+++ server/app.js
@@ -10,15 +10,22 @@ const generateRandomUser = () => {
     address: faker.address.streetAddress()
   }
 }
+const initialItems = [...Array(100)].map(() => generateRandomUser());
 
 app.get('/', (_, res) => {
   const html = fs.readFileSync('public/index.html').toString('utf-8')
   res.write(html)
 })
 
-app.get('/api/users', (_, res) => {
-  const initialItems = [...Array(100)].map(() => generateRandomUser());
-  res.json(initialItems)
+app.get('/api/users', (req, res) => {
+  const query = req.query.q
+
+  if (!query) {
+    return res.json(initialItems)
+  }
+
+  const filteredUsers = initialItems.filter(user => user.name.toLowerCase().includes(query.toLowerCase()))
+  return res.json(filteredUsers)
 })
 
 app.listen(PORT)
